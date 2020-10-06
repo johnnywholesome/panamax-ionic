8d78614d6861ac9c123533006677c76801474ea4
Jon Schmedt <jon.schmedt@usfoods.com>
2020-10-06 11:18:15 -0500

    triggering commit  for app flow

--- src/app/pages/lists/services/lists-resolver.service.ts
+++ src/app/pages/lists/services/lists-resolver.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, Resolve } from '@angular/router';
 import { ListEntityService } from './list-entity.service';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { tap, filter, first } from 'rxjs/operators';
 
 @Injectable()
 export class ListsResolver implements Resolve<boolean> {
     constructor(private listsService: ListEntityService) {
     }
-
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-        return this.listsService.getAll()
+
+        return this.listsService.loaded$
             .pipe(
-                map(lists => !!lists)
+                tap(loaded => {
+                    if (!loaded) {
+                        this.listsService.getAll();
+                    }
+                }),
+                filter(loaded => !!loaded),
+                first()
             );
     }
 }
